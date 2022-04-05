f6e1c266b3af60fb939b3309231dd4e7bf4e4d97
manhlab <manhtranduc1111@gmail.com>
2022-04-05 22:00:48 +0300

    upate file

--- pages/about.js
+++ pages/about.js
@@ -1,40 +1,15 @@
 import Layout from '../layouts/Main';
-import Link from 'next/link';
-import { useForm } from "react-hook-form";
-import { server } from '../utils/server'; 
-import { postData } from '../utils/services'; 
+import Link from 'next/link'; 
+import PageIntro from '../components/page-intro';
 
-const LoginPage = () => {
-  const { register, handleSubmit, errors } = useForm();
-
-  const onSubmit = async data => {
-    const res = await postData(`${server}/api/login`, {
-      email: data.email,
-      password: data.password
-    });
-  };
-
+const AboutPage = () => {
   return (
     <Layout>
-      <section className="form-page">
-        <div className="container">
-          <div className="back-button-section">
-            <Link href="/products">
-              <a><i className="icon-left"></i> Back to store</a>
-            </Link>
-            <div style={{backgroundImage: 'url(/images/subscribe.jpg)', height: '200px'}} className="subscribe__content1">
-          <div className="form-block" >
-            <h2>Join the NFT Revolution</h2>
-            <p className="form-block__description">From a founder of Zynga and some of the creative minds that brought us 'FarmVille' and 'Words With Friends', comes a team ready to unleash blockchain for gamers.</p>
-          </div>
-          </div>
-          </div>
-        </div>
-      </section>
-     
+    
+    <PageIntro />
     </Layout>
   )
 }
   
-export default LoginPage
+export default AboutPage
   
